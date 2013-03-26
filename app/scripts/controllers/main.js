6ea18d10933b21ca2942fd65713cf113d33a456b
Chad Skidmore <chad@skidmore.org>
2013-03-25 17:20:12 -0700

    Fixing for proper JSONP

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -5,14 +5,4 @@ angular.module('angularApp')
 
     $scope.names = TestService.query();
 
-    // TestService.query({}, function (response) {
-    //   console.log(response);
-    //   $scope.names = response.results;
-    // });
-
-    // $scope.names = TestService.query({}, function(data){
-    //   console.log('data');
-    //   console.log(data);
-    // });
-
   });
--- app/scripts/services/test.js
+++ app/scripts/services/test.js
@@ -4,7 +4,7 @@ angular.module('angularAppServices', ['ngResource'])
   .factory('TestService', function($resource) {
 
     return $resource('http://www.qtech.me/api/test',
-      {},
+      {callback: 'JSON_CALLBACK'},
       {
         query : {method :'JSONP', isArray : true}
       }
